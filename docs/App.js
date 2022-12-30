0bcd48a6058006531da45db5322db74c43957002
Adam Peleback <adam@gloot.com>
2022-12-31 00:10:01 +0100

    readded docs folder

---
+++ docs/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodoList from "./to-do-list";
+
+function App() {
+  return (
+    <>
+      <h1> Adam's to do list </h1>
+      <TodoList />;
+    </>
+  );
+}
+
+export default App;
